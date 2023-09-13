793992c5b1a10118c445190cd1b4939590f56c6d
Hkhang096 <2051050190khang@ou.edu.vn>
2023-09-13 22:28:58 +0700

    ..

--- QuanLiBanSach02/QuanLiBanSach02/Content/Clients/js.js
+++
@@ -1,7 +0,0 @@
-﻿function openNav() {
-    document.getElementById("mySidenav").style.width = "140px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
--- QuanLiBanSach02/QuanLiBanSach02/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLiBanSach02.MvcApplication" Language="C#" %>
